365768ee768da97aac3e10b209b00b0ff9ae5621
Jim Weirich <jim.weirich@gmail.com>
2011-04-16 22:17:47 -0400

    Character form

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,7 +2,7 @@ class CharactersController < ApplicationController
   # GET /characters
   # GET /characters.xml
   def index
-    @characters = Character.all
+    @characters = Character.order("name").all
 
     respond_to do |format|
       format.html # index.html.erb
