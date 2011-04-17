c962ade6a204f7ad4256c72266887b8e728529a8
Jim Weirich <jim.weirich@gmail.com>
2011-04-17 04:07:24 -0400

    Better tag filtering.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,11 @@
 class Character < ActiveRecord::Base
-  has_many :tags, :order => "category, value", :dependent => :destroy
+  has_many :tags, :order => "category, value", :dependent => :destroy do
+    def matching(cat)
+      find(:all, :conditions => ["category=?", cat])
+    end
+  end
 
   def aliases
-    names = tags.find(:all, :conditions => ["category=?", "aka"])
-    names.map { |tag| tag.value }
+    tags.matching("aka").map(&:value)
   end
 end
