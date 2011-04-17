6a4d39952a837883aed6a077aba40575b523ca3d
Jim Weirich <jim.weirich@gmail.com>
2011-04-17 04:02:50 -0400

    Added aliases on index

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,8 @@
 class Character < ActiveRecord::Base
   has_many :tags, :order => "category, value", :dependent => :destroy
+
+  def aliases
+    names = tags.find(:all, :conditions => ["category=?", "aka"])
+    names.map { |tag| tag.value }
+  end
 end
