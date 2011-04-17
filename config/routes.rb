1d7582edf15f3ab1eb6365e856459453d3188a87
Jim Weirich <jim.weirich@gmail.com>
2011-04-17 00:06:05 -0400

    Root is /characters

--- config/routes.rb
+++ config/routes.rb
@@ -52,6 +52,8 @@ Personography::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
 
+  root :to => "characters#index"
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
