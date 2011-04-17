cd15f65d3d7360a6fe9d55b563dea5e62a045f40
Jim Weirich <jim.weirich@gmail.com>
2011-04-16 21:25:53 -0400

    initial project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Personography::Application.config.secret_token = 'ec6c5b854c9ea9b9c7c4e400fef8ae2d4e79f985904c23a9f8ab1793f51cb711acad5aa123df6b888de5c454a5b44a40585b7daaa708f5db74a51ccc73a04d33'
