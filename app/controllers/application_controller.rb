bdac72e692ebc1e047008dd3f5304cd061ee441b
Jim Weirich <jim.weirich@gmail.com>
2011-09-11 17:05:32 -0400

    Clear current user if not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,5 +36,8 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:current_user_id]) if session[:current_user_id]
+  rescue ActiveRecord::RecordNotFound => ex
+    session[:current_user_id] = nil
+    nil
   end
 end
